4ce00c60fbcfb6224a1e609dde21f52c9e4e6dda
Vyacheslav <dyaditsyn@gmail.com>
2021-05-14 11:09:12 +0300

    load users from JSON

--- src/App.js
+++ src/App.js
@@ -8,28 +8,46 @@ import RecipesPage from './pages/RecipesPage';
 import HomePage from './pages/HomePage';
 import RecipesNavbar from './components/RecipesNavbar';
 import { Container } from 'react-bootstrap';
+import userJSON from './data/users.json';
 
 // App is the main component for our Recipe app
 // State:
 // activeUser: Either null, if no user is logged in, or a user object if a user is logged in
 
 class App extends React.Component {
+
   constructor(props){
-    super(props)
+    super(props);
+    let usersData;
+    if ( localStorage['allUsers']) {usersData=localStorage['allUsers'];}
+    else {usersData=userJSON}
     this.state = {
-      // activeUser: null
-      activeUser: {
-        id: 1,
-        name: 'Yaron',
-        email: 'skarlinski@gmail.com',
-        pwd: '123'
-      }
+      allUsers: usersData,
+      activeUser: null
+      // activeUser: {
+      //   id: 1,
+      //   name: 'Yaron',
+      //   email: 'skarlinski@gmail.com',
+      //   pwd: '123'
+      // }
     }
+  }
+signup = (newUser) => {
+  this.setState({
+    activeUser: newUser,
+    allUsers: this.state.allUsers.concat(newUser)
+  })
+  localStorage['allUsers'] = localStorage['allUsers'].push(newUser); //will not work bacouse local storage is string
+}
+  login = (userObj) => {
+    this.setState({
+      activeUser: userObj
+    });
   }
   logout = () => {
     this.setState({
       activeUser: null,
-    })
+    });
   }
     render() { 
       return (
@@ -44,7 +62,10 @@ class App extends React.Component {
             <HomePage></HomePage>
           </Route>
           <Route exact path="/login">
-            <Login></Login>
+            <Login 
+              allUsers={this.state.allUsers} 
+              login={this.login}
+            />
           </Route>
           <Route exact path="/signup">
             <Signup></Signup>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -5,6 +5,43 @@ import { Link } from 'react-router-dom';
 class Login extends React.Component {
     constructor(props){
         super(props)
+        this.state = {
+            email: '',
+            pwd: ''
+        }
+    }
+    updatePass  = (event) => {
+        this.setState({
+            email: event.target.value
+        })
+    }
+    updateEmail  = (event) => {
+        this.setState({
+            pwd: event.target.value
+        })
+    }
+    handleLogin = () => {
+        // Checks the typed username and password from the controlled components
+        // check if user exists in this.props.allUsers, and if the password is correct
+        // if true returns the user onject
+        // if false - show alert - password / user not found
+        const foundUser = this.props.allUsers.find( (user) => {
+           return user.email === this.state.email && user.pwd === this.state.pwd;
+        });
+        //alternatively for syntax
+        // for(let i=0; i<this.props.allUsers.length; i++){
+            // if(user.email === this.state.email && user.pwd === this.state.pwd){
+             //   foundUser = yhis.props.allUsers[i];
+             //   break;
+         //   }
+      //  }
+        if(foundUser) {
+            this.props.login(foundUser);
+            window.location.href = '/#/recipes';
+        }
+        else{
+            alert(" Incorrect Email or password");
+        }
     }
     render() {
         return (
@@ -16,22 +53,22 @@ class Login extends React.Component {
                         Email
                         </Form.Label>
                         <Col sm={10}>
-                        <Form.Control type="email" placeholder="Email" />
+                        <Form.Control type="email" placeholder="Email" onChange={this.updatePass} value={this.state.email}/>
                         </Col>
                     </Form.Group>
 
-                    <Form.Group as={Row} controlId="formHorizontalPassword">
+                    <Form.Group as={Row} controlId="formHorizontalPassword" >
                         <Form.Label column sm={2}>
                         Password
                         </Form.Label>
                         <Col sm={10}>
-                        <Form.Control type="password" placeholder="Password" />
+                        <Form.Control type="password" placeholder="Password" onChange={this.updateEmail} value={this.state.pwd}/>
                         </Col>
                     </Form.Group>
                     
                     <Form.Group as={Row}>
                         <Col sm={{ span: 10, offset: 2 }}>
-                        <Button variant="success" type="button">Log in</Button>
+                        <Button onClick={this.handleLogin} variant="success" type="button">Log in</Button>
                         </Col>
                     </Form.Group>
                 </Form>
